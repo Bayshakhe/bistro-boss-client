bdb0121ff39b688254b4968554b117241ac6c7f1
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-06-01 03:34:35 +0600

    Successfully added firebase with this projects

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const Header = () => {
+  const {user} = useContext(AuthContext)
+  // console.log(user)
   const navItems = (
     <>
       <li>
